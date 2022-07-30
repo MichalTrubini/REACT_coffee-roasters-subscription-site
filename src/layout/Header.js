efdaf6cc30836882014511490c335819d806479b
Michal Trubini <mchl.trubini@gmail.com>
2022-07-30 21:37:00 +0200

    tablet css home page WIP

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -7,8 +7,13 @@ const Header = () => {
     return ( 
         <header className="header margin-fix">
             <nav className="header__nav">
-                <a href='/'><img src={logo} alt="coffeeroasters" className='header__logo'/></a>
-                <img src={hamburger} alt="menu"/>
+                <a href='/' ><img src={logo} alt="coffeeroasters" className='header__logo'/></a>
+                <img className='header__hamburger' src={hamburger} alt="menu"/>
+                <ul className='header__list'>
+                    <li className='header__list-item'>Home</li>
+                    <li className='header__list-item'>About Us</li>
+                    <li className='header__list-item'>Create Your Plan</li>
+                </ul>
             </nav>
         </header>
      );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,6 @@ import Steps from "../components/Steps";
 import Button from "../components/Button";
 
 const Home = () => {
-
   const collection = [
     {
       id: 1,
@@ -35,24 +34,24 @@ const Home = () => {
 
   const highlights = [
     {
-        id: 1,
-        img: "./images/home/desktop/icon-coffee-bean.svg",
-        title: "Best quality",
-        text: "Discover an endless variety of the world’s best artisan coffee from each of our roasters.",
-      },
-      {
-        id: 2,
-        img: "./images/home/desktop/icon-gift.svg",
-        title: "Exclusive benefits",
-        text: "Special offers and swag when you subscribe, including 30% off your first shipment.",
-      },
-      {
-        id: 3,
-        img: "./images/home/desktop/icon-truck.svg",
-        title: "Free shipping",
-        text: "We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.",
-      }
-  ]
+      id: 1,
+      img: "./images/home/desktop/icon-coffee-bean.svg",
+      title: "Best quality",
+      text: "Discover an endless variety of the world’s best artisan coffee from each of our roasters.",
+    },
+    {
+      id: 2,
+      img: "./images/home/desktop/icon-gift.svg",
+      title: "Exclusive benefits",
+      text: "Special offers and swag when you subscribe, including 30% off your first shipment.",
+    },
+    {
+      id: 3,
+      img: "./images/home/desktop/icon-truck.svg",
+      title: "Free shipping",
+      text: "We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.",
+    },
+  ];
 
   return (
     <main>
@@ -68,35 +67,56 @@ const Home = () => {
         <div className="collection__list">
           {collection.map((item) => (
             <div className="collection__item" key={item.id}>
-              <img
-                className="collection__img"
-                src={item.img}
-                alt={item.title}
-              />
-              <h3 className="collection__title">{item.title}</h3>
-              <p className="collection__text">{item.text}</p>
+              <div className="collection__img-container">
+                <img
+                  className="collection__img"
+                  src={item.img}
+                  alt={item.title}
+                />
+              </div>
+              <div className="collection__content-container">
+                <h3 className="collection__title">{item.title}</h3>
+                <p className="collection__text">{item.text}</p>
+              </div>
             </div>
           ))}
         </div>
       </section>
       <section className="highlights margin-fix">
         <div className="highlights__intro">
-            <h2 className="highlights__header">Why choose us?</h2>
-            <p className="highlights__text">A large part of our role is choosing which particular coffees will be featured in our range. This means working closely with the best coffee growers to give you a more impactful experience on every level.</p>
+          <h2 className="highlights__header">Why choose us?</h2>
+          <p className="highlights__text">
+            A large part of our role is choosing which particular coffees will
+            be featured in our range. This means working closely with the best
+            coffee growers to give you a more impactful experience on every
+            level.
+          </p>
         </div>
         <div className="highlights__items">
-            {highlights.map(item => 
-                <div className="highlights__item" key={item.title}>
-                    <img className="highlights__item-img" src={item.img} alt={item.title}/>
-                    <h3 className="highlights__item-header">{item.title}</h3>
-                    <p className="highlights__item-text">{item.text}</p>
-                </div>
-            )}
+          {highlights.map((item) => (
+            <div className="highlights__item" key={item.title}>
+              <div className="highlights__img-container">
+                <img
+                  className="highlights__item-img"
+                  src={item.img}
+                  alt={item.title}
+                />
+              </div>
+              <div className="highlights__content-container">
+                <h3 className="highlights__item-header">{item.title}</h3>
+                <p className="highlights__item-text">{item.text}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </section>
       <section className="steps__home">
         <h2 className="steps__home-header">How it works</h2>
-        <Steps classNameSteps='steps__margin-fix' classNameHeader='steps__item-title-dark' classNamePar='steps__item-text-dark'/>
+        <Steps
+          classNameSteps="steps__margin-fix"
+          classNameHeader="steps__item-title-dark"
+          classNamePar="steps__item-text-dark"
+        />
         <Button>Create your plan</Button>
       </section>
     </main>
