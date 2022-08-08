f273aa7c0c8217d4234fe6e2f32294db26d5cd01
Michal Trubini <mchl.trubini@gmail.com>
2022-08-08 07:59:13 +0200

    fixes and bugs

--- src/layout/MobileMenu.js
+++ src/layout/MobileMenu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 import '../assets/styles/css/mobileMenu.css';
 import Header from './Header';
@@ -14,17 +15,25 @@ const MobileMenu = (props) => {
         dispatch (visibilityActions.isVisible())
     }
 
+    const mobileMenuPortal = () => {
+        return ( 
+            <>
+            <div className="mobile-menu">
+                <Header src={props.src}/>
+                <ul className='mobile-menu__list'>
+                    <li><Link to={'/'} className='mobile-menu__list-item' onClick={showMenuHandler}>Home</Link></li>
+                    <li><Link to={'/about'} className='mobile-menu__list-item' onClick={showMenuHandler}>About Us</Link></li>
+                    <li><Link to={'/subscription'} className='mobile-menu__list-item' onClick={showMenuHandler}>Create Your Plan</Link></li>
+                </ul>
+            </div>
+            <div className='mobile-menu__overlay'></div>
+            </>
+         );
+    }
+
     return ( 
         <>
-        <div className="mobile-menu">
-            <Header src={props.src}/>
-            <ul className='mobile-menu__list'>
-                <Link to={'/'} className='mobile-menu__list-item' onClick={showMenuHandler}>Home</Link>
-                <Link to={'/about'} className='mobile-menu__list-item' onClick={showMenuHandler}>About Us</Link>
-                <Link to={'/subscription'} className='mobile-menu__list-item' onClick={showMenuHandler}>Create Your Plan</Link>
-            </ul>
-        </div>
-        <div className='mobile-menu__overlay'></div>
+            {ReactDOM.createPortal(<mobileMenuPortal/>, document.getElementById('mobileMenu-root'))}
         </>
 
      );
